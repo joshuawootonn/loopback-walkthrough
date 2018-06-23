9b8fcae110f7dd634226dc771fb89a85a5d995e1
Joshua Wootonn <joshuawootonn@gmail.com>
2018-06-23 09:21:31 -0500

    Added new orphaned branch with Part one code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Loopback Walk Through
+This is a project I made to explain how loopback. 
+
+## Articles
+1. [➰Loopback models and relations]()
+
+
+## Branches 
+1. [➰Loopback models and relations]()
---
+++ server/scripts/create-lb-credential-tables.js
@@ -0,0 +1,7 @@
+var server = require('../server');
+var ds = server.dataSources.db;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er; 
+  ds.disconnect();
+});
